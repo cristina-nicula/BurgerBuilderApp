d3f8f1596058700c854b914982894d591f567e8c
cristina <cristina.nicula22@gmail.com>
2020-11-17 09:19:14 +0100

    Checkout form validity

--- src/shared/utility.js
+++ src/shared/utility.js
@@ -7,6 +7,9 @@ export const updateObject = (oldObject, updatedProperties) => {
 
 export const checkValidity = (value, rules) => {
   let isValid = true;
+  if (!rules) {
+    return true;
+  }
   if (rules.required) {
     isValid = value.trim() !== "" && isValid;
   }
@@ -17,5 +20,16 @@ export const checkValidity = (value, rules) => {
   if (rules.maxLength) {
     isValid = value.length <= rules.maxLength && isValid;
   }
+
+  if (rules.isEmail) {
+    const pattern = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
+    isValid = pattern.test(value) && isValid;
+  }
+
+  if (rules.isNumeric) {
+    const pattern = /^\d+$/;
+    isValid = pattern.test(value) && isValid;
+  }
+
   return isValid;
 };
